16c41feaf05244e7000b7a4d7e2d5a6ec3fc2d5c
simiadi <simonadam006@gmail.com>
2022-12-02 07:50:40 +0100

    switch/case

---
+++ 06 - Elágazások (switch-case)/Feladat 03/main.py
@@ -0,0 +1,22 @@
+from os import system
+
+udito: int = None
+
+print("Üdítők:\nCoca Cola --> 1\nPepsi --> 2\nFanta --> 3\nSprite --> 4\n", end="")
+
+print("Hányas számű üdítőt szeretnéd? ", end="")
+udito = int(input())
+
+system("cls")
+
+match udito:
+    case 1:
+        print("Sikeres vásárlás! (Coca Cola)", end="")
+    case 2:
+        print("Sikeres vásárlás! (Pepsi)", end="")
+    case 3:
+        print("Sikeres vásárlás! (Fanta)", end="")
+    case 4:
+        print("Sikeres vásárlás! (Sprite)", end="")
+    case _:
+        print("Nincs ilyen üdítő", end="")
---
+++ 06 - Elágazások (switch-case)/Feladat 04/main.py
@@ -0,0 +1,36 @@
+from os import system
+
+szam1: int = None
+szam2: int = None
+muvelet: str = None
+eredmeny1: float = None
+eredmeny2: float = None
+eredmeny3: float = None
+eredmeny4: float = None
+
+print("Adj meg egy számot: ", end="")
+szam1 = int(input())
+
+print("Adj meg egy másik számot: ", end="")
+szam2 = int(input())
+
+print("Milyen műveletet szeretnél elvégezni? (+,-,*,/) ", end="")
+muvelet = str(input())
+
+system("cls")
+
+match muvelet:
+    case "+":
+        eredmeny1 = szam1 + szam2
+        print(f"Eredmény: {eredmeny1}", end="")
+    case "-":
+        eredmeny2 = szam1 - szam2
+        print(f"Eredmény: {eredmeny2}", end="")
+    case "*":
+        eredmeny3 = szam1 * szam2
+        print(f"Eredmény: {eredmeny3}", end="")
+    case "/":
+        eredmeny4 = szam1 / szam2
+        print(f"Eredmény: {eredmeny4}", end="")
+    case _:
+        print("Nincs ilyen művelet", end="")
