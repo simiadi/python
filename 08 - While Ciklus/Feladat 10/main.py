b91392ec85281d2621971544aedd7ce897706d33
simiadi <simonadam006@gmail.com>
2023-02-15 07:42:38 +0100

    while

--- 08 - While Ciklus/Feladat 10/main.py
+++ 08 - While Ciklus/Feladat 10/main.py
@@ -2,9 +2,11 @@ number: int = None
 temp: str = None
 isNumber: bool = False
 truncateString: str = None
+numbers: int = None
+count: int = 0
 
 while (number == None or number > 99 or number < 1):
-    print("Adjon meg egy számot: ", end="")
+    print("Adjon meg egy maximum kétjegyű pozitív egész számot: ", end="")
     temp = input()
     truncateString = temp.replace(".", "").replace("-", "")
     isNumber = truncateString.isnumeric()
@@ -13,11 +15,22 @@ while (number == None or number > 99 or number < 1):
         number = int(temp)
         if number > 99 or number < 1:
             print("A megadott szám nem egyjegyű vagy kétjegyű pozitív egész szám!")
+        else:
+            for numbers in range(1,number):
+                if numbers % 2 == 0:
+                    print(numbers)
+            print("")
+            for numbers in range(1,number):
+                if numbers % 5 == 0:
+                    print(numbers)
+            print("")
+            for numbers in range(1,number):
+                if numbers % 11 == 0:
+                    count += 1
+            print(count)
+            print("")
+            for numbers in range(1,number):
+                if numbers % 7 == 3:
+                    print(numbers)
     else:
         print("Nem számot adott meg!")
-
-if number % 7 == 0:
-    print("A megadott szám osztható 7-tel!")
-else:
-    print("A megadott szám nem osztható 7-tel!")
-    
