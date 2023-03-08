bd2d593be438c6c54f53a439ed4ab7449795aea0
simiadi <simonadam006@gmail.com>
2023-03-08 07:20:55 +0100

    lehivas

--- 09 - Függvények/Feladat 01/main.py
+++ 09 - Függvények/Feladat 01/main.py
@@ -3,10 +3,22 @@ from mathFunctions import *
 
 x: float = None
 y: float = None
-result: float = None
+sumresult: float = None
+kivonasresult: float = None
+szorzasresult: float = None
+osztasresult: float = None
 
 x = getNumberFromConsole()
 y = getNumberFromConsole()
 
-result = sumOfTwoNumbers(x, y)
-printToConsole(x, y, result, "+")
+sumresult = sumOfTwoNumbers(x, y)
+printToConsole(x, y, sumresult, "+")
+
+kivonasresult = sumOfTwoNumbers(x, y)
+printToConsole(x, y, kivonasresult, "-")
+
+szorzasresult = sumOfTwoNumbers(x, y)
+printToConsole(x, y, szorzasresult, "*")
+
+osztasresult = sumOfTwoNumbers(x, y)
+printToConsole(x, y, osztasresult, "/")
